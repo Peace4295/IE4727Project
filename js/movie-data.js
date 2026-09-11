09964d74928c5eb63336c02b84caa0a34246e067
nlimxe-boop <nlimxe@gmail.com>
2026-09-11 14:28:53 +0000

    Updated movie-data.js

--- js/movie-data.js
+++ js/movie-data.js
@@ -7,7 +7,7 @@ const movies = [
         language: "English",
         subtitles: "English",
         duration: "2h 3m",
-        poster: "images/jumanji.png",
+        poster: "images/movie/jumanji.png",
         status: "showing",
 
         synopsis: "When Spencer goes back into the fantastical world of Jumanji, pals Martha, Fridge and Bethany re-enter the game to bring him home. But the game is now broken -- and fighting back. Everything the friends know about Jumanji is about to change, as they soon discover there are more obstacles and more danger to overcome.",
@@ -23,7 +23,7 @@ const movies = [
         language: "English",
         subtitles: "English",
         duration: "2h 2m",
-        poster: "images/joker.png",
+        poster: "images/movie/joker.png",
         status: "showing",
         
         synopsis: "Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a futile attempt to feel like he's part of the world around him. Isolated, bullied and disregarded by society, Fleck begins a slow descent into madness as he transforms into the criminal mastermind known as the Joker.",
@@ -39,7 +39,7 @@ const movies = [
         language: "English",
         subtitles: "English",
         duration: "1h 31m",
-        poster: "images/minionposter.jpg",
+        poster: "images/movie/minionposter.jpg",
         status: "showing",
         
         synopsis: "Evolving from single-celled yellow organisms at the dawn of time, Minions live to serve, but find themselves working for a continual series of unsuccessful masters, from T. Rex to Napoleon. Without a master to grovel for, the Minions fall into a deep depression. But one minion, Kevin, has a plan; accompanied by his pals Stuart and Bob, Kevin sets forth to find a new evil boss for his brethren to follow. Their search leads them to Scarlet Overkill, the world's first-ever super-villainess.",
@@ -55,7 +55,7 @@ const movies = [
         language: "English",
         subtitles: "English",
         duration: "2h 23m",
-        poster: "images/avengers.png",
+        poster: "images/movie/avengers.png",
         status: "showing",
         
         synopsis: "When Thor's evil brother, Loki, gains access to the unlimited power of the energy cube called the Tesseract, Nick Fury, director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth. Joining Fury's &quotdream team&quot are Iron Man, Captain America, the Hulk, Thor, the Black Widow and Hawkeye.",
@@ -71,7 +71,7 @@ const movies = [
         language: "English",
         subtitles: "English",
         duration: "1h 48m",
-        poster: "images/zootopia.png",
+        poster: "images/movie/zootopia.png",
         status: "showing",
         
         synopsis: "From the largest elephant to the smallest shrew, the city of Zootopia is a mammal metropolis where various animals live and thrive. When Judy Hopps becomes the first rabbit to join the police force, she quickly learns how tough it is to enforce the law. Determined to prove herself, Judy jumps at the opportunity to solve a mysterious case. Unfortunately, that means working with Nick Wilde, a wily fox who makes her job even harder.",
